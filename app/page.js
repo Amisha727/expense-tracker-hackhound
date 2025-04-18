037230d77147d22e030348b61ac8c4b4a31a73fa
meenalsingh0 <0603meenalsingh@gmail.com>
2025-04-18 15:54:52 +0530

    path corrected

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 'use client'
-import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
+
+import HeroSection from "./_components/HeroSection";
+import Navbar from "./_components/Navbar";
+import About from './_components/About';
 export default function Home() {
   return (
     <div>
